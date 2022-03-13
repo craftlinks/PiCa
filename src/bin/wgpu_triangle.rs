d7092d55f4f1f79c0bbf8f6f6ee3d031e8b612c1
craftlinks <craftlinks@protonmail.com>
2022-03-13 21:08:41 +0100

    202203132108

--- src/bin/wgpu_triangle.rs
+++ src/bin/wgpu_triangle.rs
@@ -1,3 +1,21 @@
-pub fn main() {
-    println!("Hello wgpu triangles");
+use PiCa::error::Error;
+use PiCa::pica_window::{Window, WindowAttributes};
+
+pub fn main() -> Result<(), Error> {
+    let instances = wgpu::Instance::new(wgpu::Backends::all());
+    for adapter in instances.enumerate_adapters(wgpu::Backends::all()) {
+        println!("{:?}", adapter.get_info());
+    }
+
+    let window_attributes = WindowAttributes::new()
+        .with_title("Awesome PiCa Simulation")
+        .with_position(50, 50)
+        .with_size(800, 800);
+
+    let mut window = Window::new_with_attributes(window_attributes)?;
+    while window.pull() {
+        window.push();
+    }
+
+    Ok(())
 }
