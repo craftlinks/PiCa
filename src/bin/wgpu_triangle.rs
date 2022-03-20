ebc2d116da7fd03fc6a424025b4d4c473c6ad3ca
craftlinks <craftlinks@protonmail.com>
2022-03-20 21:51:11 +0100

    points and lines example

--- src/bin/wgpu_triangle.rs
+++ src/bin/wgpu_triangle.rs
@@ -1,14 +1,11 @@
+use std::borrow::Cow;
 use PiCa::error::Error;
 use PiCa::pica_window::{Window, WindowAttributes};
-use PiCa::wgpu_renderer::WGPURenderer;
-use wgpu::RenderPipeline;
+use PiCa::wgpu_renderer::{Inputs, WGPURenderer};
 
 pub fn main() -> Result<(), Error> {
-    let instances = wgpu::Instance::new(wgpu::Backends::all());
-    for adapter in instances.enumerate_adapters(wgpu::Backends::all()) {
-        println!("{:?}", adapter.get_info());
-    }
 
+    // Create a PiCa window
     let window_attributes = WindowAttributes::new()
         .with_title("Awesome PiCa Simulation")
         .with_position(50, 50)
@@ -16,76 +13,27 @@ pub fn main() -> Result<(), Error> {
 
     let mut window = Window::new_with_attributes(window_attributes)?;
 
+    // Logging for wgpu
     env_logger::init();
 
-    // WGPU initialization
-    let mut wgpu_renderer = pollster::block_on(WGPURenderer::wgpu_init(window.as_ref()));
+    // Define the inputs for the WGPURenderer
+    let inputs = Inputs {
+        source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("../../assets/shader.wgsl"))),
+        topology: wgpu::PrimitiveTopology::TriangleList,
+        strip_index_format: None,
+    };
 
-    // WGPU application dependent code
-    let pipeline_layout = wgpu_renderer.device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-        label: None,
-        bind_group_layouts: &[], // placeholder, vertext and color data are written directly in the shader
-        push_constant_ranges: &[],
-    });
-
-    let render_pipeline = wgpu_renderer.device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-        label: None,
-        layout: Some(&pipeline_layout),
-        vertex: wgpu::VertexState {
-            module: &wgpu_renderer.shader,
-            entry_point: "vs_main",
-            buffers: &[],
-        },
-        fragment: Some(wgpu::FragmentState {
-            module: &wgpu_renderer.shader,
-            entry_point: "fs_main",
-            targets: &[wgpu_renderer.texture_format.into()],
-        }),
-        primitive: wgpu::PrimitiveState::default(),
-        depth_stencil: None,
-        multisample: wgpu::MultisampleState::default(),
-        multiview: None,
-    });
+    // WGPURenderer initialization
+    let mut wgpu_renderer = pollster::block_on(WGPURenderer::wgpu_init(window.as_ref(), inputs));
 
     // PiCa window rendering loop
     while window.pull() {
         window.push();
-        render(&mut wgpu_renderer, &render_pipeline);
+        
+        // Paint to the window surface
+        wgpu_renderer.render(3);
     }
 
+    // All was well
     Ok(())
-}
-
-pub fn render(wgpu_renderer: &mut WGPURenderer, render_pipeline: &RenderPipeline) {
-    // Later should just take a closure
-    let frame = wgpu_renderer.surface.get_current_texture().unwrap();
-    let view = frame
-        .texture
-        .create_view(&wgpu::TextureViewDescriptor::default());
-    let mut encoder =
-        wgpu_renderer.device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
-    {
-        let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-            label: None,
-            color_attachments: &[wgpu::RenderPassColorAttachment {
-                view: &view,
-                resolve_target: None,
-                ops: wgpu::Operations {
-                    load: wgpu::LoadOp::Clear(wgpu::Color {
-                        r: 0.05,
-                        g: 0.062,
-                        b: 0.08,
-                        a: 1.0,
-                    }),
-                    store: true,
-                },
-            }],
-            depth_stencil_attachment: None,
-        });
-        rpass.set_pipeline(render_pipeline);
-        rpass.draw(0..3, 0..1);
-    }
-    wgpu_renderer.queue.submit(Some(encoder.finish()));
-    frame.present();
-
 }
--- src/lib/wgpu_renderer.rs
+++ src/lib/wgpu_renderer.rs
@@ -1,6 +1,13 @@
 use std::borrow::Cow;
 
-use crate::pica_window::{self, Window};
+use wgpu::{ShaderSource, PrimitiveTopology, IndexFormat};
+use crate::pica_window::Window;
+
+pub struct Inputs<'a> {
+    pub source: ShaderSource<'a>,
+    pub topology: PrimitiveTopology,
+    pub strip_index_format: Option<IndexFormat>,
+}
 
 pub struct WGPURenderer {
     pub device: wgpu::Device,
@@ -8,10 +15,14 @@ pub struct WGPURenderer {
     pub texture_format: wgpu::TextureFormat,
     pub queue: wgpu::Queue,
     pub shader: wgpu::ShaderModule,
+    pub render_pipeline: wgpu::RenderPipeline,
 }
 
 impl WGPURenderer {
-    pub async fn wgpu_init(window: &Window) -> WGPURenderer {
+    pub async fn wgpu_init(
+        window: &Window,
+        inputs: Inputs<'_>,
+    ) -> WGPURenderer {
         let size = window.window_attributes.size;
         let instance = wgpu::Instance::new(wgpu::Backends::DX12);
         let surface = unsafe { instance.create_surface(window) };
@@ -47,10 +58,42 @@ impl WGPURenderer {
         // Load the shaders from disk
         let shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
             label: None,
-            source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!(
-                "../../assets/shader.wgsl"
-            ))),
+            source: inputs.source,
         });
+
+        // WGPU application dependent code
+        let pipeline_layout =
+            device
+                .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                    label: None,
+                    bind_group_layouts: &[], // placeholder, vertext and color data are written directly in the shader
+                    push_constant_ranges: &[],
+                });
+
+        let render_pipeline =
+            device
+                .create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                    label: None,
+                    layout: Some(&pipeline_layout),
+                    vertex: wgpu::VertexState {
+                        module: &shader,
+                        entry_point: "vs_main",
+                        buffers: &[],
+                    },
+                    fragment: Some(wgpu::FragmentState {
+                        module: &shader,
+                        entry_point: "fs_main",
+                        targets: &[texture_format.into()],
+                    }),
+                    primitive: wgpu::PrimitiveState{
+                        topology: inputs.topology,
+                        strip_index_format: inputs.strip_index_format,
+                        ..Default::default()
+                    },
+                    depth_stencil: None,
+                    multisample: wgpu::MultisampleState::default(),
+                    multiview: None,
+                });
 
         let wgpu_renderer = WGPURenderer{
             device,
@@ -58,9 +101,43 @@ impl WGPURenderer {
             texture_format,
             queue,
             shader,
+            render_pipeline,
         };
 
         wgpu_renderer
     }
 
+    pub fn render(&mut self, num_vertices: usize) {
+        // Later should just take a closure
+        let frame = self.surface.get_current_texture().unwrap();
+        let view = frame
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+        {
+            let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: None,
+                color_attachments: &[wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.05,
+                            g: 0.062,
+                            b: 0.08,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                }],
+                depth_stencil_attachment: None,
+            });
+            rpass.set_pipeline(&self.render_pipeline);
+            rpass.draw(0..num_vertices as u32, 0..1);
+        }
+        self.queue.submit(Some(encoder.finish()));
+        frame.present();
+}
 }
